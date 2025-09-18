20e7947196b4a6df30ada9a8d7f4b2700fe340f3
Aldy Imam Pratama <aldy.i.p43@gmail.com>
2025-09-18 09:31:15 +0700

    Refactor code style and improve compatibility with PHP 8.2
    - Updated class definitions to use braces for class bodies.
    - Changed exit statements to use lowercase 'or' for consistency.
    - Added #[\AllowDynamicProperties] attribute to classes to prevent dynamic property creation in PHP 8.2+.
    - Adjusted various conditional statements to use consistent spacing and formatting.
    - Improved readability by aligning concatenation operators and ensuring consistent use of parentheses.
    - Updated comments and documentation for clarity.

--- system/core/Model.php
+++ system/core/Model.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * CodeIgniter
  *
@@ -36,7 +37,7 @@
  * @since	Version 1.0.0
  * @filesource
  */
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /**
  * Model Class
@@ -47,7 +48,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * @author		EllisLab Dev Team
  * @link		https://codeigniter.com/userguide3/libraries/config.html
  */
-class CI_Model {
+class CI_Model
+{
 
 	/**
 	 * Class constructor
@@ -73,5 +75,4 @@ class CI_Model {
 		//	most likely a typo in your model code.
 		return get_instance()->$key;
 	}
-
 }
