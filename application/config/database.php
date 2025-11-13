cbeb90a6519dcfbc9cea946cfe8b84c90e1c1ebf
Aldy Imam Pratama <aldy.i.p43@gmail.com>
2025-11-13 13:44:26 +0700

    fix(config): update database connection settings for remote access

--- application/config/database.php
+++ application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
+	'hostname' => '192.168.51.40',
+	'username' => 'remote',
+	'password' => 'Automation@321',
 	'database' => 'air_system',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
