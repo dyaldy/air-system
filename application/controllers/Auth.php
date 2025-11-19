03d897fdb17adbd15bcd0c4679ce001da3d77bcc
Aldy Imam Pratama <aldy.i.p43@gmail.com>
2025-11-19 10:09:20 +0700

    fix(Auth): correct model name casing for consistency

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -29,7 +29,7 @@ class Auth extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        $this->load->model('user_model');
+        $this->load->model('User_model');
         $this->load->helper('common');
     }
 
